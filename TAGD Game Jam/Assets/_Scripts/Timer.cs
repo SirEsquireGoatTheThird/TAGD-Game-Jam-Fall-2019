d635f835b4923f3495a9bf9cc3cfa29f23d28d24
murbina7631 <56103471+murbina7631@users.noreply.github.com>
2019-10-26 15:30:16 -0500

    Timer

---
+++ TAGD Game Jam/Assets/_Scripts/Timer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float duration;
+    public Image fillImage;
+
+    public void Update()
+    {
+        if ((int)Time.time % duration == 0)
+        {
+            fillImage.fillAmount = 1f;
+            StartCoroutine(timer(duration));
+        }
+        Debug.Log(Time.time);
+    }
+
+    public IEnumerator timer(float duration)
+    {
+        float startTime = Time.time;
+        float time = duration;
+        float value = 0;
+
+        while (Time.time - startTime < duration)
+        {
+            time -= Time.deltaTime;
+            value = time / duration;
+            fillImage.fillAmount = value;
+            yield return null;
+        }
+    }
+}
