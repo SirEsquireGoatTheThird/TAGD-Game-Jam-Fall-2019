c63bb0a967f384039f1ccbab633e5615fe23b8f5
willjfish <williamjacksonfisher@gmail.com>
2019-11-21 00:42:24 -0600

    Implemented More Sounds
    Game transitions to intense version of song if song ends at bandits or above
    
    Added UI SFX
    Added Sounds to Win and Lose screen

--- TAGD Game Jam/Assets/_Scripts/SongHandler.cs
+++ TAGD Game Jam/Assets/_Scripts/SongHandler.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class SongHandler : MonoBehaviour
 {
+    private int count;
+
 
     public AudioClip song_1;
+    public AudioClip song_2;
     AudioSource audioSource_Song;
 
     void Start()
     {
+        count = 0;
+        GameManager.Instance.NextEnemy.AddListener(update_count);
         audioSource_Song = GetComponent<AudioSource>();
         audioSource_Song.volume = .8F;
     }
@@ -18,8 +23,23 @@ public class SongHandler : MonoBehaviour
     {
         if (!audioSource_Song.isPlaying)
         {
-            audioSource_Song.clip = song_1;
-            audioSource_Song.Play();
+            if(count < 3)
+            {
+                audioSource_Song.clip = song_1;
+                audioSource_Song.Play();
+            }
+            else
+            {
+                audioSource_Song.clip = song_2;
+                audioSource_Song.Play();
+            }
+
         }
     }
+
+    void update_count()
+    {
+        count++;
+    }
+
 }
